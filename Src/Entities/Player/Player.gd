70ee413cd116cb619b9a443767f8bdfa37f79076
Tom <tom.rushton@hotmail.co.uk>
2021-07-15 00:21:35 +0100

    Add walk and jump controls to player

--- Src/Entities/Player/Player.gd
+++ Src/Entities/Player/Player.gd
@@ -1,9 +1,23 @@
 extends KinematicBody2D
 
 const MAX_FALL_SPEED = 500
-const GRAVITY_ACCELERATION = 10
+const GRAVITY_ACCELERATION = 20
+const WALK_SPEED = 200
+const JUMP_SPEED = -700
 
 var velocity := Vector2.ZERO
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = WALK_SPEED
+	else:
+		velocity.x = 0
+		
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_SPEED
 
 
 func _physics_process(delta: float) -> void:
